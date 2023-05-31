91929ee2951cf276de96be5fa046071612ee6fc3
Darrell Tan <darrelltwh@gmail.com>
2023-05-31 19:01:42 +0800

    Added Base UI

--- src/main/java/com/example/firstprogram/Main.java
+++ src/main/java/com/example/firstprogram/Main.java
@@ -1,8 +1,10 @@
 package com.example.firstprogram;
 import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+
 
 public class Main extends Application {
     @Override
@@ -25,9 +27,6 @@ public class Main extends Application {
     }
 
 
-    public static void main(String[] args) {
-        launch(args);
-    }
     public static void main(String[] args) {
         launch(args);
     }
